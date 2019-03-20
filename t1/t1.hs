21b9fdefc1772442c48cbc57e6d07171db73bf2f
lorenzofmann <lorenzofman@gmail.com>
2019-03-20 14:49:53 -0300

    Add t1 folder and haskell operators pdf

---
+++ t1/t1.hs
@@ -0,0 +1,12 @@
+-- 1)
+isVowel :: Char -> Bool
+isVowel char =
+  char == 'a' ||
+  char == 'e' ||
+  char == 'i' ||
+  char == 'u' ||
+  char == 'A' ||
+  char == 'E' ||
+  char == 'I' ||
+  char == 'O' ||
+  char == 'U' 
