0adb5c055842538db01e99ed694bcdcb3825c08a
lorenzofmann <lorenzofman@gmail.com>
2019-04-08 06:20:27 -0300

    Improve pattern1 readability

--- t2/Patterns.hs
+++ t2/Patterns.hs
@@ -2,7 +2,12 @@ module Patterns where
 import Constants
 import Types
 pattern1 :: Int -> Int -> Float -> Float -> Float -> [Rectangle]
-pattern1 n m width height offset = [(((i*(width+offset), (j*(height+offset))), (width,height) )) | j <- [0..fromIntegral (m-1)], i <- [0..fromIntegral (n-1)]]
+pattern1 n m width height offset = [((x i, y j), (width,height)) | j <- columnIt, i <- lineIt]
+    where
+        x i = i*(width+offset)
+        y j = j*(height+offset)
+        lineIt = [0..fromIntegral (n-1)]
+        columnIt = [0..fromIntegral (m-1)]
 
 pattern2 :: Int -> Float -> Float -> Float -> [Rectangle]
 pattern2 n width height radius = [((x i,y i),(width,height)) | i <- [0 .. fromIntegral n]]
