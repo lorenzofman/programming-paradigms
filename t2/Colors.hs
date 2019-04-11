962fdfe089cfde58612546aec2784d383ffba85a
lorenzofmann <lorenzofman@gmail.com>
2019-04-11 06:09:02 -0300

    Add HSV2RGB conversion function

--- t2/Colors.hs
+++ t2/Colors.hs
@@ -1,9 +1,27 @@
 module Colors where
+    import Data.Fixed
     type RGBColor = (Int,Int,Int)
-    type HSVColor = (Int,Int,Int)
+    type HSVColor = (Int,Float,Float)
 
-    convertRGB2HSL :: RGBColor -> HSVColor
-    convertRGB2HSL = undefined
-
-    convertHSL2RGB :: HSVColor -> RGBColor
-    convertHSL2RGB = undefined
+    --https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+    convertHSV2RGB :: HSVColor -> RGBColor
+    convertHSV2RGB (h,s,v) = (round r,round g,round b) where
+        r = (r' + m) * 255
+        g = (g' + m) * 255
+        b = (b' + m) * 255
+        m = v - c
+        c = v * s
+        (r',g',b') = convertHSV2RGBAux (h,s,v)
+    convertHSV2RGBAux :: HSVColor -> (Float,Float,Float)
+    convertHSV2RGBAux (h,s,v)
+        | h >= 000  && h < 060 = (c,x,0)
+        | h >= 060  && h < 120 = (x,c,0)
+        | h >= 120  && h < 180 = (0,c,x)
+        | h >= 180  && h < 240 = (0,x,c)
+        | h >= 240  && h < 300 = (x,0,c)
+        | otherwise = (c,0,x) where
+            c = v * s
+            x = c * (1 - y)
+            y = abs (z - 1)
+            z = mod' w 2.0
+            w = (fromIntegral h)/ 60.0
