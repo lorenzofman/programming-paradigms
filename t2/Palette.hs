722e792270ca63590fe51f87c7968514eb20e153
lorenzofmann <lorenzofman@gmail.com>
2019-04-11 06:15:23 -0300

    Add new palette generator

--- t2/Palette.hs
+++ t2/Palette.hs
@@ -1,6 +1,11 @@
     module Palette where
-    rgbPalette :: Int -> [(Int,Int,Int)]
-    rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
+        import Colors
+        rgbPalette :: Int -> [RGBColor]
+        rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
 
-    greenPalette :: Int -> [(Int,Int,Int)]
-    greenPalette n = [(0,80+i*10,0) | i <- [0..n] ]
+        greenPalette :: Int -> [RGBColor]
+        greenPalette n = [(0,80+i*10,0) | i <- [0..n] ]
+
+        colorWheel :: Int -> Float -> Float -> [RGBColor]
+        colorWheel n s v = map convertHSV2RGB [(h i,s,v) | i <- [0..n] ] where
+            h i = quot (i * 360) n
