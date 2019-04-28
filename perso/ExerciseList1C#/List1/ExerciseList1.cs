9d8cc0e4a1096253faa8c38c5fb9c362314532a3
lorenzofmann <lorenzofman@gmail.com>
2019-04-28 16:41:41 -0300

    Add HasEqheads function, SuperList and SpaceCount

--- perso/ExerciseList1C#/List1/ExerciseList1.cs
+++ perso/ExerciseList1C#/List1/ExerciseList1.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /// <summary>
-/// If you use Visual Studio you can use C# Interactive to use just like ghci
+/// If you use Visual Studio you can use C# Interactive to have a ghci C# version, access it in View -> Other Windows -> C# Interactive
+/// References: 
+///		https://blog.submain.com/csharp-functional-programming/
+///		https://www.tabsoverspaces.com/233633-head-and-tail-like-methods-on-list-in-csharp-and-fsharp-and-python-and-haskell
+///		
 /// </summary>
 namespace List1
 {
@@ -17,5 +21,12 @@ namespace List1
 		/// The syntax of a func in very similar to the way we do in Haskell, we get an integer and another one and transform in a 
 		/// </summary>
 		private static Func<int, int, int> SumSquares = (x, y) => x * x + y * y;
+
+		private static Func<List<int>, List<int>, bool> HasEqualHeads = (x, y) => x.First() == y.First();
+
+		private static Func<List<string>, List<string>> SuperList = (xs) => xs.Select(x => "Super" + x).ToList();
+
+		private static Func<string, int> SpaceCount = (str) => str.Where(x => x == ' ').Count();
+
 	}
 }
