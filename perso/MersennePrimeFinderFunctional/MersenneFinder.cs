6f12d1cea96460507f77e28ceab560c4388fda05
lorenzofmann <lorenzofman@gmail.com>
2019-05-01 09:34:41 -0300

    Add functional method which decides if a given number is prime or not. It's not the best in terms of optimization but it follows the functional paradigm

--- perso/MersennePrimeFinderFunctional/MersenneFinder.cs
+++ perso/MersennePrimeFinderFunctional/MersenneFinder.cs
@@ -8,8 +8,11 @@ namespace MersennePrimeFinderFunctional
 {
 	public class MersenneFinder
 	{
-
-		private static Func<UInt64, UInt64> CalculateMersennePrime = index =>
+		private static Func<int, bool> IsPrime = number =>
+		{
+			return number > 1 && Enumerable.Range(2, number - 2).Where(n => number % n == 0).Count() == 0;
+		};
+		private static Func<int, int> CalculateMersennePrime = index =>
 		{
 			return 0;
 		};
@@ -17,7 +20,7 @@ namespace MersennePrimeFinderFunctional
 		{
 			Console.WriteLine("Mersenne index: ");
 			string input = Console.ReadLine();
-			Console.WriteLine("Result: " + CalculateMersennePrime(Convert.ToUInt64(input)));
+			Console.WriteLine("Result: " + CalculateMersennePrime(Convert.ToInt32(input)));
 		}
 	}
 }
