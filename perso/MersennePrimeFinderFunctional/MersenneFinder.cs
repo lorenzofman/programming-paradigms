f9da329c427d18b25569923db2973a76f6c63ff8
lorenzofmann <lorenzofman@gmail.com>
2019-05-01 09:17:02 -0300

    Add mersenne prime finder project

---
+++ perso/MersennePrimeFinderFunctional/MersenneFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MersennePrimeFinderFunctional
+{
+	public class MersenneFinder
+	{
+
+		private static Func<UInt64, UInt64> CalculateMersennePrime = index =>
+		{
+			return 0;
+		};
+		protected static void Main() 
+		{
+			Console.WriteLine("Mersenne index: ");
+			string input = Console.ReadLine();
+			Console.WriteLine("Result: " + CalculateMersennePrime(Convert.ToUInt64(input)));
+		}
+	}
+}
