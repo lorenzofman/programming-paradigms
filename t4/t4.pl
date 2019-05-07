df5fb713f7599776ca48814377980ef5dad1fef2
lorenzofmann <lorenzofman@gmail.com>
2019-05-07 20:42:32 -0300

    Solve the mistery, update some typos and update the logic with the new scenario teacher gave us

--- t4/t4.pl
+++ t4/t4.pl
@@ -1,15 +1,17 @@
-assassin(WHO) :- reason(WHO), access(WHO).
+assassin(WHO) :- reason(WHO), access(WHO), !.
     % Assassin predicates
-    reason(WHO) :- jealous(maria,WHO); insane(WHO); money(WHO).
+    reason(WHO) :- jealous(WHO,anita); insane(WHO); money(WHO).
+    % Everybody was in the apartment
     access(WHO) :- key(WHO), weapon(WHO).
         % Access predicates
-        key(WHO) :- track(WHO,sm,wednesday) ; track(WHO,poa,tuesday).
+        % Bia has a key no matter where she had been
+        key(WHO) :- track(WHO,sm,monday) ; track(WHO,poa,tuesday) ; WHO = bia.
         weapon(WHO) :- baseballBat(WHO) ; hammer(WHO).
             % Weapon predicates
             baseballBat(WHO) :- track(WHO,poa,thursday) ; track(WHO,sm,wednesday).
             hammer(WHO) :- track(WHO,ap,wednesday) ; track(WHO,ap,thursday).
         % Reason predicates
-        jealous(X,Y) :- relation(X,Z),  relation(Z,Y).
+        jealous(X,Y) :- relation(X,Z),  relation(Y,Z), X \= Y.
         money(WHO) :- poor(WHO).
     % General predicates
         % relation(W,M).
@@ -19,8 +21,6 @@ assassin(WHO) :- reason(WHO), access(WHO).
     % What Hercule Poirot know?
     insane(adriano).
     insane(maria).
-
-    % Bia has a key no matter where she had been
 
     % Relations (W,M)
     relation(anita,bernardo).
@@ -44,43 +44,43 @@ assassin(WHO) :- reason(WHO), access(WHO).
     track(pedro,ap,friday).
 
     track(caren,poa,monday).
-    track(caren,poa,thursday).
+    track(caren,poa,tuesday).
     track(caren,poa,wednesday).
     track(caren,sm,thursday).
     track(caren,ap,friday).
 
     track(henrique,ap,monday).
-    track(henrique,poa,thursday).
+    track(henrique,poa,tuesday).
     track(henrique,ap,wednesday).
     track(henrique,ap,thursday).
     track(henrique,ap,friday).
 
     track(bia,ap,monday).
-    track(bia,poa,thursday).
+    track(bia,poa,tuesday).
     track(bia,poa,wednesday).
     track(bia,sm,thursday).
     track(bia,ap,friday).
 
     track(adriano,ap,monday).
-    track(adriano,ap,thursday).
+    track(adriano,ap,tuesday).
     track(adriano,poa,wednesday).
     track(adriano,ap,thursday).
     track(adriano,ap,friday).
 
     track(alice,ap,monday).
-    track(alice,poa,thursday).
+    track(alice,poa,tuesday).
     track(alice,poa,wednesday).
     track(alice,ap,thursday).
     track(alice,ap,friday).
 
     track(bernardo,sm,monday).
-    track(bernardo,sm,thursday).
+    track(bernardo,sm,tuesday).
     track(bernardo,poa,wednesday).
     track(bernardo,sm,thursday).
     track(bernardo,ap,friday).
 
     track(maria,ap,monday).
-    track(maria,sm,thursday).
+    track(maria,sm,tuesday).
     track(maria,sm,wednesday).
     track(maria,sm,thursday).
     track(maria,ap,friday).
